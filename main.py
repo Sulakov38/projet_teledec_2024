6d0a801125a42ed5fec50266a39915cb4d62fdd1
Pierre-abm <pierre.abraham.etu@gmail.com>
2024-12-02 15:53:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,52 @@
 import geopandas as gpd
-
-# Charger le fichier shapefile
-shapefile_path_vege = "FORMATION_VEGETALE.shp"
-shapefile_path_emprise = "emprise_etude.shp"
-
-gdf_vegetation = gpd.read_file(shapefile_path_vege)
-gdf_emprise = gpd.read_file(shapefile_path_emprise)
-
-# Ajouter les champs "nom" et "code" avec des valeurs par défaut
-gdf_vegetation['nom'] = None
-gdf_vegetation['code'] = None
-
-# Mise à jour des champs pour la code spécifique
-
-# Création d'un dictionnaire pour mapper les valeurs de 'TFV' aux colonnes 'nom' et 'code'
-mapping = {
-    "FF1-49-49": ("Autres feuillus", "11"),
-    "FF1-10-10": ("Autres feuillus", "11"),
-    "FF1-09-09": ("Autres feuillus", "11"),
-    "FF1G01-01": ("chêne", "12"),
-    "FF1-14-14": ("Robinier", "13"),
-    "FP": ("Peupleraie", "14"),
-    "FF1-00-00": ("Mélange de feuillus", "15"), 
-    "FF1-00": ("Feuillus en îlots", "16"),
-    "FF2-91-91": ("Autres conifères autre que pin", "21"),
-    "FF2-63-63": ("Autres conifères autre que pin", "21"),
-    "FF2G61-61": ("Autres conifères autre que pin", "21"),
-    "FF2-90-90": ("Autres conifères autre que pin", "21"),
-    "FF2-81-81": ("Autres Pin", "22"),
-    "FF2-52-52": ("Autres Pin", "22"),
-    "FF2-80-80": ("Autres Pin", "22"),
-    "FF2-64-64": ("douglas", "23"),
-    "FF2G53-53": ("Pin laricio ou pin noir", "24"),
-    "FF2-51-51": ("Pin maritime", "25"),
-    "FF2-00-00": ("Mélange conifères", "26"),
-    "FF2-00": ("Conifères en îlots", "27"),
-    "FF32": ("Mélange de conifères prépondérants et feuillus", "28"),
-    "FF31": ("Mélange de feuillus prépondérants et conifères", "29"),
-}
-
-# Application des valeurs du mapping
-gdf_vegetation['nom'] = gdf_vegetation['CODE_TFV'].map(lambda x: mapping[x][0] if x in mapping else None)
-gdf_vegetation['code'] = gdf_vegetation['CODE_TFV'].map(lambda x: mapping[x][1] if x in mapping else None)
-
-gdf_vegetation = gdf_vegetation.dropna(subset=['code'])
-
-
-vege_emprise = gpd.clip(gdf_vegetation, gdf_emprise)
-# Sauvegarder les modifications dans un nouveau fichier (ou remplacer l'original)
-output_path = "FORMATION_VEGETALE_MODIFIE.shp"
-vege_emprise.to_file(output_path)
+def code_vege(shapefile_path_vege,shapefile_path_emprise,output_path)
+   
+    # Charger le fichier shapefile
+    gdf_vegetation = gpd.read_file(shapefile_path_vege)
+    gdf_emprise = gpd.read_file(shapefile_path_emprise)
+    
+    # Ajouter les champs "nom" et "code" avec des valeurs par défaut
+    gdf_vegetation['nom'] = None
+    gdf_vegetation['code'] = None
+    
+    # Mise à jour des champs pour la code spécifique
+    
+    # Création d'un dictionnaire pour mapper les valeurs de 'TFV' aux colonnes 'nom' et 'code'
+    mapping = {
+        "FF1-49-49": ("Autres feuillus", "11"),
+        "FF1-10-10": ("Autres feuillus", "11"),
+        "FF1-09-09": ("Autres feuillus", "11"),
+        "FF1G01-01": ("chêne", "12"),
+        "FF1-14-14": ("Robinier", "13"),
+        "FP": ("Peupleraie", "14"),
+        "FF1-00-00": ("Mélange de feuillus", "15"), 
+        "FF1-00": ("Feuillus en îlots", "16"),
+        "FF2-91-91": ("Autres conifères autre que pin", "21"),
+        "FF2-63-63": ("Autres conifères autre que pin", "21"),
+        "FF2G61-61": ("Autres conifères autre que pin", "21"),
+        "FF2-90-90": ("Autres conifères autre que pin", "21"),
+        "FF2-81-81": ("Autres Pin", "22"),
+        "FF2-52-52": ("Autres Pin", "22"),
+        "FF2-80-80": ("Autres Pin", "22"),
+        "FF2-64-64": ("douglas", "23"),
+        "FF2G53-53": ("Pin laricio ou pin noir", "24"),
+        "FF2-51-51": ("Pin maritime", "25"),
+        "FF2-00-00": ("Mélange conifères", "26"),
+        "FF2-00": ("Conifères en îlots", "27"),
+        "FF32": ("Mélange de conifères prépondérants et feuillus", "28"),
+        "FF31": ("Mélange de feuillus prépondérants et conifères", "29"),
+    }
+    
+    # Application des valeurs du mapping
+    gdf_vegetation['nom'] = gdf_vegetation['CODE_TFV'].map(lambda x: mapping[x][0] if x in mapping else None)
+    gdf_vegetation['code'] = gdf_vegetation['CODE_TFV'].map(lambda x: mapping[x][1] if x in mapping else None)
+    
+    gdf_vegetation = gdf_vegetation.dropna(subset=['code'])
+    
+    
+    vege_emprise = gpd.clip(gdf_vegetation, gdf_emprise)
+    # Sauvegarder les modifications dans un nouveau fichier (ou remplacer l'original)
+    
+    vege_emprise.to_file(output_path)
 
 
